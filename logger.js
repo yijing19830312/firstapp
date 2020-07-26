07669430985727cfbaf1a8498bf67c86b3eb9be6
yijing19830312 <yijingz@cn.ibm.com>
2020-07-26 08:57:22 +0800

    update

---
+++ logger.js
@@ -0,0 +1,15 @@
+const EventEmmiter = require('events');
+
+var url = 'http://mylogger.io/log';
+
+class Logger extends EventEmitter {
+    log(message) {
+        //Send an HTTP request
+        console.log(message);
+    
+        this.emit('messageLogged', {id : 1, url: 'http://'});
+    }
+}
+
+
+module.exports= Logger;
